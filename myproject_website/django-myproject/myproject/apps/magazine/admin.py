b3efc531a4602a249190f0a2333dc953611148e4
mszahan <mszahan1@gmail.com>
2022-06-02 19:39:17 +0600

    Defined overwritable app settings

--- myproject_website/django-myproject/myproject/apps/magazine/admin.py
+++ myproject_website/django-myproject/myproject/apps/magazine/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django import forms 
 
-# Register your models here.
+from .models import NewsArticle
+
+from .app_settings import ARTICLE_THEME_CHOICES
+
+# Note that we could have defined the choices for.....
+#  the theme field in models.py, but instead we created a custom ModelForm......
+#   in administration and set the choices there. This
+#  was done to avoid the creation of new database migrations whenever the ARTICLE_THEME_CHOICES is changed.
+
+class NewsArticleModelForm(forms.ModelForm):
+    theme = forms.ChoiceField(
+        label=NewsArticle._meta.get_field('theme').verbose_name,
+        choices=ARTICLE_THEME_CHOICES,
+        required=not NewsArticle._meta.get_field('theme').blank,
+        
+        )
+    class Meta:
+        fields = "__all__"
